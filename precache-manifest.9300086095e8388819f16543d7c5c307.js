dbf8758490e23bce007912d95e309143fa304f42
Sai Kumar <saikumaradapa20@gmail.com>
2024-06-24 13:37:35 +0530

    Updates

---
+++ precache-manifest.9300086095e8388819f16543d7c5c307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef25846c4a02dc7ece91aa8522a53f42",
+    "url": "/saikumar/index.html"
+  },
+  {
+    "revision": "9a9e2218e42777a171df",
+    "url": "/saikumar/static/css/2.0733074b.chunk.css"
+  },
+  {
+    "revision": "8522c15470bbd0c9ac5d",
+    "url": "/saikumar/static/css/main.161a37c0.chunk.css"
+  },
+  {
+    "revision": "9a9e2218e42777a171df",
+    "url": "/saikumar/static/js/2.8c0b4894.chunk.js"
+  },
+  {
+    "revision": "8522c15470bbd0c9ac5d",
+    "url": "/saikumar/static/js/main.eb067f9e.chunk.js"
+  },
+  {
+    "revision": "97bd71e9bf39a1f4bf62",
+    "url": "/saikumar/static/js/runtime~main.c7194287.js"
+  }
+]);
